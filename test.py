771b53c142cfdb7303513ed51d38db102778ca5f
jinwo <jinwoocho7370@gmail.com>
2021-05-21 15:24:51 +0900

    v_1.0

--- test.py
+++ test.py
@@ -1,9 +1,1 @@
-from ppadb.client import Client as AdbClient
-from com.dtmilano.android.viewclient import ViewClient
-
-client = AdbClient(host="127.0.0.1", port=5037)
-devices = client.devices()
-device = devices[0]
-
-vc = ViewClient(*ViewClient.connectToDeviceOrExit())
-vc.traverse()
+print("hello")
